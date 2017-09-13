0f3fc7d9fd0530f467c85f0e3f52d404c6095bbd
Sanjeeyammal <31921337+Sanjeeyammal@users.noreply.github.com>
2017-09-13 16:12:39 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the num");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n even number");
+}
+else{
+printf("\n odd number");
+}
